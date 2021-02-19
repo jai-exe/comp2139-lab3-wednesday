b6ffa4a8d1bae916e8e643c0776516c9cc6c6a9c
Jai Kumar <kjai1903@gmail.com>
2021-02-18 20:01:04 -0500

    Lab 4 updates

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,99 @@
+﻿using comp2139_lab3_wednesday.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace comp2139_lab3_wednesday.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactContext context { get; set; }
+
+        public ContactController(ContactContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var contact = context.Contacts
+                .Include(c => c.Category)
+                .FirstOrDefault(c => c.ContactId == id);
+            return View(contact);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Categories = context.Categories.OrderBy(c => c.Name).ToList();
+            return View("Edit", new Contact());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Categories = context.Categories.OrderBy(c => c.Name).ToList();
+
+            var contact = context.Contacts
+                .Include(c => c.Category)
+                .FirstOrDefault(c => c.ContactId == id);
+            
+            return View(contact);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var contact = context.Contacts
+                .Include(c => c.Category)
+                .FirstOrDefault(c => c.ContactId == id);
+
+            return View(contact);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Contact contact)
+        {
+            string action = (contact.ContactId == 0) ? "Add" : "Edit";
+
+            if (ModelState.IsValid)
+            {
+                if (action == "Add")
+                {
+                    contact.DateAdded = DateTime.Now;
+                    context.Contacts.Add(contact);
+                }
+                else
+                {
+                    context.Contacts.Update(contact);
+                }
+
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = action;
+                ViewBag.Categories = context.Categories.OrderBy(c => c.Name).ToList();
+                return View(contact);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Contact contact)
+        {
+            context.Contacts.Remove(contact);
+            context.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+
+
+
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using comp2139_lab3_wednesday.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace comp2139_lab3_wednesday.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ContactContext context { get; set; }
+
+        public HomeController(ContactContext ctx)
+        {
+            context = ctx;
+        }
+
+
+        public IActionResult Index()
+        {
+            var contacts = context.Contacts
+                           .Include(c => c.Category)
+                           .OrderBy(c => c.Firstname).ToList();
+
+            return View(contacts);
+        }
+    }
+}
